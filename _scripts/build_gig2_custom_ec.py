e03bbd21497f46c1671bc0030aa0e213b3cb82fd
nuuuwan <nuuuwan@gmail.com>
2024-02-21 10:32:01 +0530

    Added gig2_custom_ec_only script

---
+++ _scripts/build_gig2_custom_ec.py
@@ -0,0 +1,46 @@
+import os
+
+from utils import File, Log
+
+log = Log('build_gig2_custom_ec')
+
+DIR_GIG2 = os.path.join('gig2')
+DIR_GIG2_CUSTOM = os.path.join('gig2_custom_ec_only')
+
+
+def init():
+    if not os.path.exists(DIR_GIG2_CUSTOM):
+        os.makedirs(DIR_GIG2_CUSTOM)
+
+
+def is_pd_id(x: str):
+    return x.startswith('EC-') and len(x) == 6
+
+
+def extract_lines(lines: list):
+    return lines[:1] + [
+        line for line in lines[1:] if is_pd_id(line.partition('\t')[0])
+    ]
+
+
+def process_file(file_name: str):
+    file_path = os.path.join(DIR_GIG2, file_name)
+    lines = File(file_path).read_lines()
+    new_lines = extract_lines(lines)
+
+    new_file_path = os.path.join(DIR_GIG2_CUSTOM, file_name)
+    File(new_file_path).write_lines(new_lines)
+
+    n = len(new_lines)
+    log.info(f'Wrote {n} lines to {new_file_path}')
+
+
+def main():
+    init()
+    for file_name in os.listdir(DIR_GIG2):
+        if file_name.startswith('government-elections'):
+            process_file(file_name)
+
+
+if __name__ == "__main__":
+    main()
