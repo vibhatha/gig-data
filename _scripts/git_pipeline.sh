3af3554f035732ecfe8568f56261cd54501ae51e
nuuuwan <nuuuwan@gmail.com>
2024-06-13 08:28:51 +0530

    Added _scripts/git_pipeline.sh

---
+++ _scripts/git_pipeline.sh
@@ -0,0 +1,19 @@
+git add ents
+git commit -m "Updated ents"
+
+git add gig2
+git commit -m "Updated gig2"
+
+git add gig2_custom_ec_only
+git commit -m "Updated gig2_custom_ec_only"
+
+git add _tmp
+git commit -m "Updated _tmp"
+
+git add geo
+git commit -m "Updated geo"
+
+git add *
+git commit -m "Updated remainder"
+
+git push origin main
